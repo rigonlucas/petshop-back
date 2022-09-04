b45dc255136d385c64311172e7ac7aa1770f793b
Lauro Rigon <lauro.rigon@odig.net>
2022-09-04 17:26:48 -0300

    corrigido rollback de migrations

--- database/migrations/2022_07_18_011419_add_account_id_to_users_table.php
+++ database/migrations/2022_07_18_011419_add_account_id_to_users_table.php
@@ -28,6 +28,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeign(['account_id']);
+            $table->dropColumn('account_id');
         });
     }
 };
--- database/migrations/2022_08_06_210439_create_schedule_histories_table.php
+++ database/migrations/2022_08_06_210439_create_schedule_histories_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pet_registers');
+        Schema::dropIfExists('schedule_histories');
     }
 };
