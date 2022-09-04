0cbb93e978be336bc33595b96e766b0f3f60deb9
Lucas Rigon <lucas@bussolasocial.com.br>
2022-09-04 13:52:35 -0300

    deleta produto associado ao agendamento

--- app/Http/Controllers/ScheduleProducts/ScheduleProductsDeleteController.php
+++ app/Http/Controllers/ScheduleProducts/ScheduleProductsDeleteController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\ScheduleProducts;
 
 use App\Http\Controllers\Controller;
+use App\Services\Application\ScheduleProducts\DTO\ScheduleProductDeleteData;
+use App\Services\Application\ScheduleProducts\ScheduleProductDeleteService;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ScheduleProductsDeleteController extends Controller
 {
-    public function __invoke(Request $request, int $id, int $scheduleProductId)
-    {
-        dd($request->all(), $id, $scheduleProductId);
+    /**
+     * @throws ValidationException
+     */
+    public function __invoke(
+        Request $request,
+        int $id,
+        int $scheduleProductId,
+        ScheduleProductDeleteService $service
+    ) {
+        $data = new ScheduleProductDeleteData();
+        $data->schedule_id = $id;
+        $data->schedule_product_id = $scheduleProductId;
+        $data->account_id = $request->user()->account_id;
+
+        $service->delete($data);
+
+        return response()->noContent();
     }
 }
---
+++ app/Services/Application/ScheduleProducts/ScheduleProductDeleteService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services\Application\ScheduleProducts;
+
+use App\Models\Schedules\Schedule;
+use App\Models\Schedules\ScheduleHasProduct;
+use App\Rules\AccountHasEntityRule;
+use App\Rules\ScheduleProducts\ScheduleHasProductRule;
+use App\Services\Application\ScheduleProducts\DTO\ScheduleProductDeleteData;
+use App\Services\BaseService;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class ScheduleProductDeleteService extends BaseService
+{
+
+    /**
+     * @throws ValidationException
+     */
+    public function delete(ScheduleProductDeleteData $data): bool
+    {
+        $this->validate($data);
+        return ScheduleHasProduct::query()
+            ->where('id', '=', $data->schedule_product_id)
+            ->where('schedule_id', '=', $data->schedule_id)
+            ->delete();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    private function validate(ScheduleProductDeleteData $data): void
+    {
+        Validator::make(
+            $data->toArray(),
+            [
+                'schedule_id' => [
+                    'required',
+                    'integer',
+                    'min:1',
+                    new AccountHasEntityRule(Schedule::class, $data->account_id),
+                    new ScheduleHasProductRule($data->schedule_product_id)
+                ],
+            ]
+        )->validate();
+    }
+}
